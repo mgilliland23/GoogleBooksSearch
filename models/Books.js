3045845bb9cae0b305156cad46e2d893e114ac09
Matt Gilliland <mgilliland23@gmail.com>
2019-09-09 17:34:50 -0400

    added mongo schema

---
+++ models/Books.js
@@ -0,0 +1,40 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+var BookSchema = new Schema({
+  // `title` is required and of type String
+  title: {
+    type: String,
+    required: true
+  },
+  authors: {
+    type: Array,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  saved: {
+    type: Boolean,
+    required: true
+  }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Book = mongoose.model("Book", BookSchema);
+
+// Export the Book model
+module.exports = Book;
