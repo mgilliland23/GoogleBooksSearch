33a05412c14ce2f367f3e3ecb414c454558ddde2
Matt Gilliland <mgilliland23@gmail.com>
2019-09-09 18:23:20 -0400

    created navbar component

--- client/src/Components/Navbar/index.js
+++ client/src/Components/Navbar/index.js
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="navbar-brand">Google Books Search</div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="navbar-brand">Google Books Search</div>
       <button
-        class="navbar-toggler"
+        className="navbar-toggler"
         type="button"
         data-toggle="collapse"
-        data-target="#navbarText"
-        aria-controls="navbarText"
+        data-target="#navbarLinks"
+        aria-controls="navbarLinks"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <div class="navbar-text">Search</div>
-        <div class="navbar-text">Saved</div>
+      <div className="collapse navbar-collapse" id="navbarLinks">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link active" href="#">
+            Search <span className="sr-only">(current)</span>
+          </a>
+          <a className="nav-item nav-link" href="#">
+            Saved
+          </a>
+        </div>
       </div>
     </nav>
   );
